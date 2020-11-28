3c5aa1e4ac4dbf007a4980cb9bce616c586eec77
Abdul Salam <abdulsalamsabil@gmail.com>
2020-11-28 20:32:20 +0800

    Logic Bookingg

---
+++ TestWPPL/Booking/BookingController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Velacro.Api;
+using Velacro.Basic;
+using TestWPPL.Model;
+
+namespace TestWPPL.Booking
+{
+    public class BookingController : MyController {
+        public BookingController(IMyView _myView) : base(_myView) { 
+            
+        }
+
+        public async void booking(String token)
+        {
+            var client = new ApiClient(ApiConstant.BASE_URL);
+            var request = new ApiRequestBuilder();
+
+            var req = request
+                .buildHttpRequest()
+                .setEndpoint("api/myBooking")
+                .setRequestMethod(HttpMethod.Get);
+            client.setAuthorizationToken(token);
+            client.setOnSuccessRequest(setItem);
+            var response = await client.sendRequest(request.getApiRequestBundle());
+            //Console.WriteLine(response.getJObject()["token"]);
+            //client.setAuthorizationToken(response.getJObject()["access_token"].ToString());
+        }
+
+        private void setItem(HttpResponseBundle _response)
+        {
+            if (_response.getHttpResponseMessage().Content != null)
+            {
+                string status = _response.getHttpResponseMessage().ReasonPhrase;
+                Console.WriteLine("BAWAH");
+                Console.WriteLine(_response.getParsedObject<Root>().booking);
+                getView().callMethod("setBooking", _response.getParsedObject<Root>().booking);
+            }
+        }
+    }
+}
